c0c66e5723f7c7196ac39ea792d0d4def94b1561
Yuliya Gaspodarik <gaspod.yu@gmail.com>
2020-06-12 04:04:49 +0300

    Correct script

--- lesson-20-ajax-spa/AJAXStorage.js
+++ lesson-20-ajax-spa/AJAXStorage.js
@@ -1,63 +1,14 @@
-function TAjaxStorage(name) {
-  var self = this;
-  self.storage = {};
-}
+'use strict';
 
 var ajaxHandlerScript = "http://fe.it-academy.by/AjaxStringStorage2.php";
 var updatePassword;
 
-TAjaxStorage.prototype.sendRequest = function () {
-  $.ajax(
-    {
-      url: ajaxHandlerScript,
-      type: 'POST',
-      data: {
-        f: 'LOCKGET', n: 'GASPODARIK_DRINK_STORAGE',
-        p: updatePassword
-      },
-      cache: false,
-      success: getReady,
-      error: errorHandler
-    }
-  );
-};
-
-TAjaxStorage.prototype.updateStorage = function () {
-  $.ajax(
-    {
-      url: ajaxHandlerScript,
-      type: 'POST',
-      data: {
-        f: 'UPDATE', n: 'GASPODARIK_DRINK_STORAGE',
-        v: JSON.stringify(this.storage), p: updatePassword
-      },
-      cache: false,
-      success: updateReady,
-      error: errorHandler
-    }
-  );
-
-  function updateReady(result) {
-    if (result.error !== undefined) {
-      alert(result.error);
-    }
-  }
-};
-
-function errorHandler(jqXHR, statusStr, errorStr) {
-  alert(statusStr + ' ' + errorStr);
-}
-
-function getReady(result) {
-  if (result.error !== undefined) {
-    alert(result.error);
-  } else {
-    if (result.result != '') {
-      this.storage = JSON.parse(result.result);
-    } else {
-      this.storage = {};
-    }
-  }
+function TAjaxStorage(nameStorage) {
+  /*var self = this;
+  self.storage = {};*/
+  this.name = 'GASPODARIK_' + nameStorage + '_STORAGE';
+  this.storage = {};
+  this.readStorage();
 }
 
 TAjaxStorage.prototype.addValue = function (key, value) {
@@ -66,20 +17,7 @@ TAjaxStorage.prototype.addValue = function (key, value) {
   this.storage[key] = value;
   this.updateStorage();
 };
-TAjaxStorage.prototype.readStorage = function () {
-  $.ajax(
-    {
-      url: ajaxHandlerScript,
-      type: 'POST',
-      data: {
-        f: 'READ', n: 'GASPODARIK_DRINK_STORAGE'
-      },
-      cache: false,
-      success: getReady,
-      error: errorHandler
-    }
-  );
-};
+
 TAjaxStorage.prototype.getValue = function (key) {
   this.readStorage();
   return this.storage[key];
@@ -101,3 +39,72 @@ TAjaxStorage.prototype.getKeys = function () {
   this.readStorage();
   return Object.keys(this.storage);
 };
+
+TAjaxStorage.prototype.readStorage = function () {
+  $.ajax(
+    {
+      url: ajaxHandlerScript,
+      type: 'POST',
+      data: {
+        f: 'READ', n: this.name
+      },
+      cache: false,
+      success: getReady,
+      error: errorHandler
+    }
+  );
+};
+
+TAjaxStorage.prototype.sendRequest = function () {
+  $.ajax(
+    {
+      url: ajaxHandlerScript,
+      type: 'POST',
+      data: {
+        f: 'LOCKGET', n: this.name,
+        p: updatePassword
+      },
+      cache: false,
+      success: getReady,
+      error: errorHandler
+    }
+  );
+};
+
+TAjaxStorage.prototype.updateStorage = function () {
+  $.ajax(
+    {
+      url: ajaxHandlerScript,
+      type: 'POST',
+      data: {
+        f: 'UPDATE', n: this.name,
+        v: JSON.stringify(this.storage), p: updatePassword
+      },
+      cache: false,
+      success: updateReady,
+      error: errorHandler
+    }
+  );
+
+  function updateReady(result) {
+    if (result.error !== undefined) {
+      alert(result.error);
+    }
+  }
+};
+
+function getReady(result) {
+  if (result.error !== undefined) {
+    alert(result.error);
+  } else {
+    if (result.result != '') {
+      this.storage = JSON.parse(result.result);
+    } else {
+      this.storage = {};
+    }
+  }
+}
+
+function errorHandler(jqXHR, statusStr, errorStr) {
+  alert(statusStr + ' ' + errorStr);
+}
